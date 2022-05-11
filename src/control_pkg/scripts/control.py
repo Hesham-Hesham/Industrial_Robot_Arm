6ce199dc51c84c39f1fde7fd3c0efdbb7acb92e3
Ebrahim Abdelghfar Ebrahim <81301684+ebrahimabdelghfar@users.noreply.github.com>
2022-05-11 16:32:39 +0200

    modified_control

--- src/control_pkg/scripts/control.py
+++ src/control_pkg/scripts/control.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from re import A
 from modulesdir.modules import *
 
 tau=2*pi
@@ -12,7 +11,6 @@ ABB_INFO=RobotCommander() #object that Provides information such as the robot’
 ABB_planning=planning_scene_interface #object This provides a remote interface for getting, setting, and updating the robot’s internal understanding of the surrounding world:\
 ABB_move=MoveGroupCommander("joint_group")
 display_trajectory_publisher = rospy.Publisher("/move_group/display_planned_path",moveit_msgs.msg.DisplayTrajectory,queue_size=20)
-
 #go by specify joint angle 
 def go_by_angle():
     ABB_joint_controller=ABB_move.get_current_joint_values()
@@ -26,18 +24,32 @@ def go_by_angle():
     ABB_move.stop()
 #end
 
-def go_by_pos():
+def go_by_pos(orinetation_l=list(),position_l=list()):
     pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 0.03
-    pose_goal.orientation.x = 0.36
-    pose_goal.orientation.y = -0.2109
-    pose_goal.orientation.z = 0.
-    pose_goal.position.x = 0.754
-    pose_goal.position.y =-0.48
-    pose_goal.position.z = 1.8
+    pose_goal.orientation.w = orinetation_l[3]
+    pose_goal.orientation.x = orinetation_l[0]
+    pose_goal.orientation.y = orinetation_l[1]
+    pose_goal.orientation.z = orinetation_l[2]
+    pose_goal.position.x = position_l[0]
+    pose_goal.position.y = position_l[1]
+    pose_goal.position.z = position_l[2]
     ABB_move.set_pose_target(pose_goal)
     ABB_move.go(wait=True)
     ABB_move.stop()
     ABB_move.clear_pose_targets()
-go_by_pos()
+if __name__ == "__main__":
+    while not rospy.is_shutdown():
+        position_list=[]
+        orinetation_list=[]
+        position_list.append(float(input("enter the position in x-cordinate: ")))
+        position_list.append(float(input("enter the position in y-cordinate: ")))
+        position_list.append(float(input("enter the position in z-cordinate: ")))
+        orinetation_list.append(float(input("enter the orinetation in dx : ")))
+        orinetation_list.append(float(input("enter the orientation in dy: ")))
+        orinetation_list.append(float(input("enter the orientation in dz: ")))
+        orinetation_list.append(float(input("enter the orientation in dw: ")))
+        go_by_pos(orinetation_list,position_list)
+
+
+        
     
